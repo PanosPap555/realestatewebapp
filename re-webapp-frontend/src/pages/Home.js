cdcb0f2f76a577f34eb4c4d8cc7b7d8aa3844a7e
PanosPap555 <152335740+PanosPap555@users.noreply.github.com>
2024-04-16 18:05:33 +0300

    Some viewuser modifications

--- re-webapp-frontend/src/pages/Home.js
+++ re-webapp-frontend/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./css//Home.css"
 
-export default function Home({ authenticated, setPopupLogin, setPopupRegister }) {
+export default function Home({ authenticated, setPopupLogin, setPopupRegister, username }) {
 
     const openPopupLogin = () => {
         setPopupLogin(true);
@@ -17,11 +17,13 @@ export default function Home({ authenticated, setPopupLogin, setPopupRegister })
                 <div className='explore-text'>Explore <span className='premium-text' >Premium</span> Real Estate</div>
             </div>
             {authenticated ? (
-                <></>
+                <div className='auth-welcome'>
+                    <div>Welcome aboard <span className={'auth-highlight-username'}>{username}</span>!</div>
+                </div>
             ) : (
                 <div className='not-auth-text'>
                     <div>Discover your new home or your next investment! <span onClick={openPopupRegister} className='register-an'>Sign Up!</span></div>
-                    <div>Allready a member? <span onClick={openPopupLogin} className='login-an'>Log In!</span></div>
+                    <div>Already a member? <span onClick={openPopupLogin} className='login-an'>Log In!</span></div>
                 </div>
             )}
         </div>
