19a1f30f2a94b7caefa245282a4f156aed5c06d9
sevakiko <sevakiko@hotmail.com>
2024-04-16 18:09:41 +0300

    Listing

--- re-webapp-frontend/src/components/ListingDetailsPopup.js
+++ re-webapp-frontend/src/components/ListingDetailsPopup.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import "./css/ListingDetailsPopup.css"
 
-export default function ListingDetailsPopup({ onClose }) {
+export default function ListingDetailsPopup({ onClose, title, description, price }) {
+    const [address, setAddress] = useState("");
+    const [latitude, setLatitude] = useState(0);
+    const [longitude, setLongitude] = useState(0);
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+
+    useEffect(() => {
+        fetchDetails(1);
+        
+    }, [])
+
+    const fetchDetails = async (id) => {
+        
+        console.log(id)
+        try {
+            const response = await axios.get("http://localhost:8080/details", {
+                params: {
+                    id: id
+                }
+            });
+
+            if (response.data.errorCode === null) {
+                setAddress(response.data.address);
+                setLatitude(response.data.latitude);
+                setLongitude(response.data.longitude);
+                setUsername(response.data.username);
+                setEmail(response.data.email);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
         <div className="popup listing-details-popup">
             <div className="popup-content">
@@ -9,12 +43,14 @@ export default function ListingDetailsPopup({ onClose }) {
                 <hr />
                 <div className='listing-details'>
                     <div className='listing-image'>
-                        <img src='src\Server\Pics\1.jpg' alt='Listing Image' />
+                        <img src='.\Server\Pics\1.jpg' />
                     </div>
                     <div className='listing-info'>
-                        <div className='listing-title'>Listing Title</div>
-                        <div className='listing-description'>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaasdasdfffffffffffffffffffafsafaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div>
+                        <div className='listing-title'>{address}</div>
+                        <div className='listing-description'>asdasdasf</div>
                         <div className='listing-price'>Listing Price</div>
+                        <div className='latitude'>{latitude}</div>
+                        <div className='longitude'>{longitude}</div>
                     </div>
                 </div>
                 <div className='btns'>
@@ -23,4 +59,9 @@ export default function ListingDetailsPopup({ onClose }) {
             </div>
         </div>
     );
+
+
+
 }
+
+
