c5e10b25f0db0a24449209b9a1563460f4803e5d
PanosPap555 <152335740+PanosPap555@users.noreply.github.com>
2024-04-17 15:03:12 +0300

    ViewUserListing first pass

--- re-webapp-frontend/src/pages/ViewUserListing.js
+++ re-webapp-frontend/src/pages/ViewUserListing.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Link } from "react-router-dom";
+import ListingDetailPopup from './ListingDetailPopup';
+
 
 function ViewUserListing() {
-    return (
-      <div className="button-wrapper">
-        <button>Button 1</button>
-        <button>Button 2</button>
-        <button>Button 3</button>
-      </div>
-    );
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  const togglePopup = () => {
+    setIsPopupOpen(!isPopupOpen);
+  };
+
+  return (
+    <div className="button-wrapper">
+      {isPopupOpen && <ListingDetailPopup onClose={togglePopup} />}
+      <button onClick={togglePopup}>Button 1</button>
+      <button>Button 2</button>
+      <button>Button 3</button>
+    </div>
+  );
 }
 
 export default ViewUserListing;
